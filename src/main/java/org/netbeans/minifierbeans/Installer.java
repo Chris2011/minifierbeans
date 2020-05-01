8d0d42f48c1c21e21ed282ea276eaa0c91d10df7
Chrizzly <Chrl@DESKTOP-S64A4IA.fritz.box>
2020-05-01 16:26:36 +0200

    #47 - Added installer to download the needed custom-packages archive.

---
+++ src/main/java/org/netbeans/minifierbeans/Installer.java
@@ -0,0 +1,12 @@
+package org.netbeans.minifierbeans;
+
+import org.openide.modules.ModuleInstall;
+import org.openide.windows.OnShowing;
+
+@OnShowing
+public class Installer extends ModuleInstall implements Runnable {
+    @Override
+    public void run() {
+
+    }
+}
