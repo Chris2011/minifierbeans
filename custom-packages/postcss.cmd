9f34e6c8f8e681e18c9ae06883ae13c5131d9901
yoancutillas <yoan.cutillas@gmail.com>
2024-02-26 11:49:56 +0100

    Fix postcss.cmd (replace bin\postcss directory with index.js)

--- custom-packages/postcss.cmd
+++ custom-packages/postcss.cmd
@@ -9,7 +9,7 @@ IF EXIST "%dp0%\node.exe" (
   SET PATHEXT=%PATHEXT:;.JS;=;%
 )
 
-"%_prog%"  "%dp0%\node_modules\postcss-cli\bin\postcss" %*
+"%_prog%"  "%dp0%\node_modules\postcss-cli\index.js" %*
 ENDLOCAL
 EXIT /b %errorlevel%
 :find_dp0
